6775ce5ed4ddeac8a6efa0b06a14347d653ac875
bajajku <bajajku@sheridancollege.ca>
2023-10-14 13:33:36 -0400

    Added flower images, created flower class and hardcoded flower list.

---
+++ app/src/main/java/sheridan/bajajku/flowershop/domain/Flower.kt
@@ -0,0 +1,9 @@
+package sheridan.bajajku.flowershop.domain
+
+data class Flower(
+    val id: String,
+    val label: String,
+    val price: Double,
+    val description: String,
+    val image: String
+)
